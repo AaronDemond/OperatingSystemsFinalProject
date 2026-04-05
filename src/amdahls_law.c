5eae9da534890571e0085de6e159c65a15a27e3f
walgorex <157838095+walgorex@users.noreply.github.com>
2026-04-05 14:58:56 -0300

    Update amdahls_law.c

--- src/amdahls_law.c
+++ src/amdahls_law.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include "logger.h"
 
-int main()
-{
+int main() {
     double P;
     int N;
+
+    write_log("AMDAHL", "Module started");
 
     // Input
     printf("Enter parallel fraction (0-1): ");
     scanf("%lf", &P);
+    write_log_input("AMDAHL", "parallel_fraction", P);
 
-    if (P < 0 || P > 1)
-    {
+    if (P < 0 || P > 1) {
+        write_log("AMDAHL", "Invalid parallel fraction");
         printf("Invalid input. P must be between 0 and 1.\n");
         return 1;
     }
 
     printf("Enter number of cores: ");
     scanf("%d", &N);
+    write_log_input("AMDAHL", "number_of_cores", N);
 
-    if (N < 1)
-    {
+    if (N < 1) {
+        write_log("AMDAHL", "Invalid number of cores");
         printf("Invalid input. Number of cores must be >= 1.\n");
         return 1;
     }
@@ -33,6 +37,11 @@ int main()
     printf("Serial Execution Time: 1.00\n");
     printf("Parallel Execution Time: %.2f\n", parallel_time);
     printf("Speedup: %.2fx\n", speedup);
+
+    char speedup_str[32];
+    snprintf(speedup_str, sizeof(speedup_str), "%.2fx", speedup);
+    write_log_input("AMDAHL", "speedup", speedup_str);
+    write_log("AMDAHL", "Calculation complete");
 
     return 0;
 }
