36ce8d5424b5177345803b4f0524e58db15beae5
lexciel <krushmor2.0+git@gmail.com>
2026-04-07 22:37:15 -0300

    "Removed extra flags from boot"

--- boot.sh
+++ boot.sh
@@ -6,11 +6,11 @@ echo "Cleaned old build and logs."
 mkdir -p bin logs
 echo "Created /bin and /logs directories."
 
-gcc -Wall -Wextra -o bin/logger src/logger.c
+gcc -o bin/logger src/logger.c
 if [ $? -eq 0 ]; then echo "[OK] logger"; else echo "[FAIL] logger"; exit 1; fi
 
 for module in file_management peterson memory amdahl main_menu; do
-    gcc -Wall -Wextra -o "bin/$module" "src/$module.c"
+    gcc -o "bin/$module" "src/$module.c"
     if [ $? -eq 0 ]; then
         echo "[OK] $module"
     else
